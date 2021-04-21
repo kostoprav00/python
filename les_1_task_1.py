47adc2ab9ce4a148e3f5c0f93ddd946f152a5cac
kostoprav.00 <kostoprav.00@github.com>
2021-04-22 00:22:18 +0300

    lesson1 fix

--- les_1_task_1.py
+++ les_1_task_1.py
@@ -2,7 +2,7 @@
 # https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&page-id=-UTIrcHMV39qAqkq9ub9&title=Untitled%20Diagram.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1K7kdcwn2_RUpaV1TjmCOOgc8GFbwAj_w%26export%3Ddownload
 
 inputValue = int(input('Введите трехзначное число'))
-if inputValue not in range(100, 999):
+if inputValue not in range(100, 1000):
     print('Это не трехзначное число!')
     exit(1)
 

6dbcd3656ffe613edff74eb799f79086d0e742ff
kostoprav00 <82651376+kostoprav00@users.noreply.github.com>
2021-05-09 22:05:00 +0300

    Lesson 7 (#7)
    * Задачи для 7-го урока
    
    * Задачи для 7-го урока
    
    Co-authored-by: kostoprav.00 <kostoprav.00@github.com>

---
+++ les_7_task_1.py
@@ -0,0 +1,20 @@
+# Отсортируйте по убыванию методом пузырька одномерный целочисленный массив,
+# заданный случайными числами на промежутке [-100; 100). Выведите на экран исходный и отсортированный массивы.
+
+from random import randint
+
+n = 10
+generated_array = [randint(-100, 100) for i in range(0, n)]
+print(generated_array)
+
+
+def bubble(array):
+    for run in range(n - 1):
+        for i in range(n - 1 - run):  # -run убираем сравнение с уже найденными пузырьками
+            if array[i] < array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+    return array
+
+
+print(bubble(generated_array))
+# print(sorted(generated_array))
